e0a8fa1d2e7094e452cfc5b987e6ccf125015a6a
07-kathir-mar <s.kathir737@gamil.com>
2026-03-31 00:12:34 +0530

    copy

--- associationSpecificBatchRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAssociationBatchDetails } from "../controllers/associationSpecificBatchController.js";
-
-const router = express.Router();
-
-router.get("/association-batch/:id", getAssociationBatchDetails);
-
-export default router;
